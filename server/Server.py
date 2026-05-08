894e6f35dcb0fb879f6cd63537678cdef4cd3d3a
student <e@e>
2026-05-08 11:24:48 +0200

    1

--- server/Server.py
+++ server/Server.py
@@ -1,10 +1,28 @@
 import socket
+import threading
+import json
 
 class Server:
     def __init__(self):
         self.host = "127.0.0.1"
         self.port = 5002
         self.s = socket.socket()
+
+        self.board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
+        self.currentPlayer = "X"
+
+    def boardControl(self, conn):
+        while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+
+            board = json.loads(data)
+            print(board)
 
     def listen(self):
         self.s.bind((self.host, self.port))
@@ -13,3 +31,8 @@ class Server:
         conn, addr = self.s.accept()
         print(f"[*] {addr}")
 
+        t = threading.Thread(target=self.boardControl, args=(conn,))
+        t.start()
+        t.join()
+
+server = Server()
