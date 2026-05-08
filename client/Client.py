4bdd081109408d171582889cd1b45cfa4f1f30d4
student <e@e>
2026-05-08 11:24:48 +0200

    1

--- client/Client.py
+++ client/Client.py
@@ -1,25 +1,35 @@
 from flask import Flask, render_template
-
+import socket
+import os
 
 class Client:
     def __init__(self, name, port):
         self.name = name
         self.port = port
-        self.app = Flask(self.name)
+        self.app = Flask(self.name, template_folder= os.path.dirname(os.path.abspath(__file__)) + "/templates")
+
+        self.board = [
+            ["", "5", ""],
+            ["8", "R", ""],
+            ["", "Q", ""]
+        ]
+
+        self.s = socket.socket()
+        self.s.connect(("127.0.0.1", 5002))
         self.setup_board()
 
     def setup_board(self):
         @self.app.route('/')
         def index():
-            board = [
-                ["", "", ""],
-                ["", "", ""],
-                ["", "", ""]
-            ]
-            return render_template("index.html", board=board)
+            return render_template("index.html", board=self.board)
 
-    def run(self, debug = False):
-        self.app.run(host="127.0.0.1", port=self.port,  threaded=True, processes=1, debug=debug)
+        @self.app.route('/move', methods=['POST'])
+        def move():
+            print(f"DZIALA - {self.port}")
+            return render_template("index.html", board=self.board)
+
+    def run(self, debug=False):
+        self.app.run(host="127.0.0.1", port=self.port, threaded=True, processes=1, debug=debug)
 
 if __name__ == '__main__':
     client = Client("Main", 5000)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
 import time
 from multiprocessing import Process
-
 from server.Server import Server
 from client.Client import Client
 
@@ -12,17 +10,15 @@ def start_server():
 
 def start_client(client, port):
     time.sleep(1)
-    os.chdir('client')
+    print("Starting client:", client, "on port:", port)
     client1 = Client(client, port)
     client1.run()
-    print("Starting client: " + str(client) + " on port: " + str(port))
 
 if __name__ == "__main__":
-    processes = []
-    processes.append(Process(target=start_server, args=()))
-    processes.append(Process(target=start_client, args=("Klient1", 5000)))
-    processes.append(Process(target=start_client, args=("Klient2", 5001)))
+    processes = [Process(target=start_server), Process(target=start_client, args=("Klient1", 5000)), Process(target=start_client, args=("Klient2", 5001)),]
+
     for p in processes:
         p.start()
+
     for p in processes:
         p.join()
