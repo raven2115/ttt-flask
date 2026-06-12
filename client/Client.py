a198a7e204375af61d76b39820f145f0e54db99f
raven2115 <bsgdudheus@gmail.com>
2026-06-12 10:01:36 +0200

    Update Client.py

--- client/Client.py
+++ client/Client.py
@@ -10,6 +10,95 @@ class Client:
         self.port = port
         self.symbol = symbol
 
+        self.currentPlayer = "X"
+        self.winner = None
+        self.clientsCount = 0
+
+        self.app = Flask(
+            self.name,
+            template_folder=os.path.dirname(os.path.abspath(__file__)) + "/templates"
+        )
+
+        self.board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(("127.0.0.1", 5002))
+
+        threading.Thread(target=self.receive_board, daemon=True).start()
+        self.setup_board()
+
+    def receive_board(self):
+        while True:
+            try:
+                data = self.s.recv(1024).decode()
+                if data:
+                    state = json.loads(data)
+                    self.board = state["board"]
+                    self.currentPlayer = state["currentPlayer"]
+                    self.winner = state["winner"]
+                    self.clientsCount = state["clients"]
+            except:
+                break
+
+    def setup_board(self):
+
+        @self.app.route('/')
+        def index():
+            return render_template(
+                "index.html",
+                                   board=self.board,
+                                   currentPlayer=self.currentPlayer,
+                                   winner=self.winner,
+                                   clients=self.clientsCount,
+                                   symbol=self.symbol)
+
+        @self.app.route('/move', methods=['POST'])
+        def move():
+            if self.winner:
+                return redirect('/')
+            move = request.form['move']
+            row, col = map(int, move.split(","))
+
+            data = {
+                "row": row,
+                "col": col,
+                "symbol": self.symbol
+            }
+            self.s.send(json.dumps(data).encode())
+            return redirect('/')
+
+        @self.app.route('/reset', methods=['POST'])
+        def reset():
+            data = {
+                "action": "reset"
+            }
+
+            self.s.send(json.dumps(data).encode())
+            return redirect('/')
+
+    def run(self, debug=False):
+        self.app.run(
+            host="127.0.0.1",
+            port=self.port,
+            threaded=True,
+            debug=debug
+        )
+from flask import Flask, render_template, request, redirect
+import socket
+import os
+import threading
+import json
+
+class Client:
+    def __init__(self, name, port, symbol):
+        self.name = name
+        self.port = port
+        self.symbol = symbol
+
         self.app = Flask(
             self.name,
             template_folder=os.path.dirname(os.path.abspath(__file__)) + "/templates"
