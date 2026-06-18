c7a9849047e13d7719a0e5703d745896d7f66e2b
raven2115 <bsgdudheus@gmail.com>
2026-06-18 21:31:31 +0200

    Refactor Client class and improve socket handling

--- client/Client.py
+++ client/Client.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+
 import socket
 import os
 import threading
 import json
 
-class Client:
+from network import Network
+
+class Client(Network):
     def __init__(self, name, port, symbol):
-        self.name = name
+        super().__init__(name)
         self.port = port
         self.symbol = symbol
 
@@ -14,47 +20,63 @@ class Client:
         self.winner = None
         self.clientsCount = 0
 
-        self.app = Flask(
-            self.name,
-            template_folder=os.path.dirname(os.path.abspath(__file__)) + "/templates"
-        )
+        self.winsX = 0
+        self.winsO = 0
+
+        self.app = Flask(self.name, template_folder=os.path.dirname(os.path.abspath(__file__)) + "/templates")
 
         self.board = [
             ["", "", ""],
             ["", "", ""],
             ["", "", ""]
         ]
-
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect(("127.0.0.1", 5002))
-
         threading.Thread(target=self.receive_board, daemon=True).start()
+
         self.setup_board()
+
+    def __del__(self):
+        try:
+            self.disconnect()
+            self.s.close()
+        except:
+            pass
+
+    def connect(self):
+        self.connected = True
+        self.log("Client connected")
+
+    def info(self):
+        return f"Client {self.name}"
 
     def receive_board(self):
         while True:
-            try:
-                data = self.s.recv(1024).decode()
-                if data:
-                    state = json.loads(data)
-                    self.board = state["board"]
-                    self.currentPlayer = state["currentPlayer"]
-                    self.winner = state["winner"]
-                    self.clientsCount = state["clients"]
-            except:
+            state = self.receive_json(self.s)
+
+            if state is None:
                 break
 
-    def setup_board(self):
+            self.winsX = state["winsX"]
+            self.winsO = state["winsO"]
+            self.board = state["board"]
+            self.currentPlayer = state["currentPlayer"]
+            self.winner = state["winner"]
+            self.clientsCount = state["clients"]
 
+    def setup_board(self):
         @self.app.route('/')
         def index():
             return render_template(
-                "index.html",
-                                   board=self.board,
-                                   currentPlayer=self.currentPlayer,
-                                   winner=self.winner,
-                                   clients=self.clientsCount,
-                                   symbol=self.symbol)
+            "index.html",
+            board=self.board,
+            currentPlayer=self.currentPlayer,
+            winner=self.winner,
+            clients=self.clientsCount,
+            symbol=self.symbol,
+            winsX=self.winsX,
+            winsO=self.winsO
+        )
 
         @self.app.route('/move', methods=['POST'])
         def move():
@@ -62,25 +84,21 @@ class Client:
                 return redirect('/')
             move = request.form['move']
             row, col = map(int, move.split(","))
-
             data = {
                 "row": row,
                 "col": col,
                 "symbol": self.symbol
             }
-            self.s.send(json.dumps(data).encode())
+            self.send_json(self.s, data)
             return redirect('/')
 
         @self.app.route('/reset', methods=['POST'])
         def reset():
-            data = {
-                "action": "reset"
-            }
-
-            self.s.send(json.dumps(data).encode())
+            self.send_json(self.s, {"action": "reset"})
             return redirect('/')
 
     def run(self, debug=False):
+        self.connect()
         self.app.run(
             host="127.0.0.1",
             port=self.port,
